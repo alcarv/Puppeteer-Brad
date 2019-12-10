b76798ab71111f2596c693f5c94423c0469f8bd7
alcarv <alefecarvalho@hotmail.com>
2019-12-09 23:52:45 -0300

    File bat for summon up Puppeteer

---
+++ teste.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Iniciando sistema
+ECHO ==========================================================================
+cd C:\Users\alefe\OneDrive\Documentos\Puppeteer-Brad
+node automationBra.js
+ECHO Sistema Iniciado
