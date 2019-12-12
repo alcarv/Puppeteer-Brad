29437d46e43c2361fb478e05d612fadc21afd3dc
alcarv <36234388+alcarv@users.noreply.github.com>
2019-12-12 17:19:31 -0300

    Add files via upload
    Attempts to return home without crashing software

--- automationBra.js
+++ automationBra.js
@@ -7,7 +7,7 @@ const fs = require('fs');
     // Starting browser
     const browser = await puppeteer.launch({
         headless: false,
-        executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+        //executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
         args: ['--no-sandbox', '--enable-features=NetworkService', ' --disable-setuid-sandbox ', '--ignore-certificate-errors',
             '--disable-web-security', ' --ignore-certificate-errors-spki-list '], ignoreHTTPSErrors: true
     });
@@ -26,33 +26,43 @@ const fs = require('fs');
 
     for (let index = 1; index < 1000; index++) {
         let indice = ("00" + index).slice(-3);
+        await page.waitFor(1000);
         await page.type('input[id="txtCota"]', indice);
-        const valueCota = await page.$("#txtCota");
-        const pegarCota = await (await valueCota.getProperty('value')).jsonValue();
-        const valueCpf = await page.$("#txtCpfCnpj");
-        const pegarCpf = await (await valueCpf.getProperty('value')).jsonValue();
+        let valueCota = await page.$("#txtCota");
+        let pegarCota = await (await valueCota.getProperty('value')).jsonValue();
+        let valueCpf = await page.$("#txtCpfCnpj");
+        let pegarCpf = await (await valueCpf.getProperty('value')).jsonValue();
         await page.click('img[onclick="ValidarForm();"]');
-        //console.log(page);
-        const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
-        const newPage = await newPagePromise;
-        //await newPage.setRequestInterception(true);
-        //console.log(newPage);
-        await newPage.close()
         await page.setRequestInterception(true);
         page.on('request', request => {
-            if (request.url() === 'http://bradescoconsorcios.com.br/html/content/restrito/images/canal/txt_resultadodasassembleias.gif') {
+            if (request.url() == 'http://bradescoconsorcios.com.br/html/content/restrito/images/canal/txt_resultadodasassembleias.gif' || request.url() == 'http://bradescoconsorcios.com.br/html/images/rodape_IB.gif') {
                 console.log('TA PASSANDO AQUI OOOOOG')
-                //request.abort();
                 fs.writeFile(`/Users/alefe/Documents/${pegarCpf}.txt`, pegarCota, (err) => {
                     if (err) throw err;
                     // process.exit();
                 });
-            } 
+                request.abort();
+                page.goBack();
+            } else 
             request.continue();/*else
                 request.continue();
                 page.setRequestInterception(false);*/
         });
+        //console.log(page);
+        const targetPromise = new Promise(resolve => browser.once('targetcreated', resolve));
+        const target = await targetPromise;
+        //await newPage.setRequestInterception(true);
+        //console.log(newPage);
+        try{
+            const newPage = await target.page();
+
+            
+            await newPage.close();
+        }catch(e){
+            console.log('Error: ' + e);
+        }
         await page.evaluate(() => document.getElementById("txtCota").value = "");
+
     };
 
 
