77ea8f90264f61a916c68e116320e2b58066be9b
alcarv <alefecarvalho@hotmail.com>
2019-12-15 14:22:14 -0300

    Ultimate Changes

--- automationBra.js
+++ automationBra.js
@@ -7,8 +7,7 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
         headless: false,
         executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-        args: ['--no-sandbox', '--enable-features=NetworkService', ' --disable-setuid-sandbox ', '--ignore-certificate-errors',
-            '--disable-web-security', ' --ignore-certificate-errors-spki-list ', `--window-size=${width},${height}`], ignoreHTTPSErrors: true
+        args: [`--window-size=${width},${height}`]
     });
     const page = await browser.newPage();
     await page.goto('http://bradescoconsorcios.com.br/html/scripts/LoginCotasInativas.htm');
@@ -24,7 +23,7 @@ const puppeteer = require('puppeteer');
     });
     loopCore();
     async function loopCore(){
-        for (let index = 94; index < 1000; index++) {
+        for (let index = 1; index < 1000; index++) {
             let indice = ("00" + index).slice(-3);
             await page.waitFor(500);
             await page.type('input[id="txtCota"]', indice);
@@ -34,20 +33,28 @@ const puppeteer = require('puppeteer');
             await page.click('img[onclick="ValidarForm();"]');
             await page.setRequestInterception(true);
             page.on('request', request => {
-                if (request.url() == 'http://bradescoconsorcios.com.br/html/content/restrito/images/canal/txt_resultadodasassembleias.gif' || request.url() == 'http://bradescoconsorcios.com.br/html/images/rodape_IB.gif') {
-                    console.log('TA PASSANDO AQUI OOOOOG')
-                    page.screenshot({path:`${pegarCpf}.png`});                
+                if (request.url() == 'http://bradescoconsorcios.com.br/html/content/restrito/images/canal/txt_resultadodasassembleias.gif' || request.url() == 'http://bradescoconsorcios.com.br/html/images/rodape_IB.gif') {              
+                    page.screenshot({path:`${pegarCpf}.png`});
                     request.abort();
-                    page.goBack();
-                    page.waitFor(2000);
-                    loopCore();
-                } else 
-                request.continue();/*else
+                    page.goBack(5000);
+                    //console.log('TA PASSANDO AQUI OOOOOG')
+                } else {
+                request.continue();
+                }/*else
                     request.continue();
                     page.setRequestInterception(false);*/
             });
             //console.log(page);
-            const targetPromise = new Promise(resolve => browser.once('targetcreated', resolve));
+           // await popup();
+           await page.waitForSelector('#txtCota')
+           await page.waitFor(2000);
+           await page.evaluate(() => document.getElementById("txtCota").value = "");
+
+        };
+    }; 
+
+   /* async function popup() {
+          const targetPromise = new Promise(resolve => browser.once('targetcreated', resolve));
         const target = await targetPromise;
         //await newPage.setRequestInterception(true);
         //console.log(newPage);
@@ -62,12 +69,7 @@ const puppeteer = require('puppeteer');
             //await page.evaluate(() => document.getElementById("txtCota").value = "");
             //console.log('AQUIIIIIIIIIIIIIIIIIii')
             await page.evaluate(() => document.getElementById("txtCota").value = "");
-          }
-        };
-    }; 
-
-   /* async function popup() {
-        
+        }
     
     } */
 })();
